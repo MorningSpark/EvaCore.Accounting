cbcbbdd6fbd4390825146e47d473377292ce9f36
redhadware <richyprg@gmail.com>
2025-05-04 19:27:46 -0500

    adicion de funcionalida de transaccion

---
+++ src/EvaCore.Accounting.Api/Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+
+using EvaCore.Accounting.Application.Commands.Transactions.CreateTransaction;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvaCore.Accounting.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TransactionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Create a new accounting entry
+        /// </summary>
+        /// <param name="command">The command containing the details of the accounting entry to create</param>
+        /// <returns>The ID of the created accounting entry</returns>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTransactionCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
